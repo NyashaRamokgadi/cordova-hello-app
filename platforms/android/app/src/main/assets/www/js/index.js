03d850273db95f07e0121ccd877bb65dbe54ea65
NyashaRamokgadi <nflex1637@hotmail.com>
2020-12-18 09:56:27 +0530

    new project

---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    document.getElementById('message').innerHTML = 'Welcome To Cross Platform App Development';
+}
